444923ca55b00a2bf009a689c665be6eec2a32e9
Dominik Eisl <dominik.eisl@tum.de>
2020-12-21 21:45:13 +0100

    Add preprocessing function

--- playground.py
+++ playground.py
@@ -1,16 +1,5 @@
-from preprocessing import remove_emojis
+from preprocessing import preprocess_data
 
-# open the files
-human_file = open("data/human_text.txt", 'r')
-robot_file = open("data/robot_text.txt", 'r')
+human_data = preprocess_data("data/human_text.txt")
+robot_data = preprocess_data("data/robot_text.txt")
 
-# read the files
-human_text = human_file.read()
-robot_text = robot_file.read()
-
-# create list of sentences
-human_sentences = human_text.split("\n")
-robot_sentences = robot_text.split("\n")
-
-print(type(human_sentences))
-    
