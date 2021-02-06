38ca610039a1f85c531d1cf80a9c3b28a80b8bd8
ge49doq <aldi.topalli@tum.de>
2021-02-06 21:04:26 +0100

    removed token

--- chatbot/bot/credentials.py
+++ chatbot/bot/credentials.py
@@ -5,5 +5,5 @@ This file is mainly to contain the token and the credentials with which you can 
 For safety reasons we will not be sharing them. If you want to reproduce the chatbot, ask us to give them to you:
 Contact me at: topalli@in.tum.de
 """
-bot_token = "1435954063:AAG864kTpS8tFXkpaJpZsQZHG49MeUMS2_8"
+bot_token = "normally token would be here"
 bot_user_name = "seminar2021_bot"
